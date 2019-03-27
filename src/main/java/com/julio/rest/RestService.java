c0555544b2887a2c92f38a1192d681ff08658685
Julio_Leme <julioromao3@gmail.com>
2019-03-26 22:04:26 -0300

    Versao 1.6 - criacao de VOs e code clean

--- src/main/java/com/julio/rest/RestService.java
+++ src/main/java/com/julio/rest/RestService.java
@@ -2,24 +2,37 @@ package com.julio.rest;
 
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.julio.helper.HelperUtils;
 import com.julio.vo.ContaVO;
 
+
 //http://localhost:8080/teste-ucb-web/rest/service/hello
 @Path("/service")
-@Service
 public class RestService {
+	
+	@Inject
+	private ServiceApplication tutorialService;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getById() {
+		final List<ContaVO> tutorials = tutorialService.getAllContas();
+		
+		return Response.ok(tutorials).build();
+	}
 
 	@GET
 	@Path("/{param}")
